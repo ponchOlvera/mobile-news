9c9bf59a93c7dc032ea1a69d1f6f8714e5a6bade
Veronica Hinojosa <veronica.hinojosa@VeronicaHinojosas-MacBook-Pro.local>
2022-02-24 10:02:56 -0600

    Add model mappers

--- app/src/main/java/com/wizeline/mobilenews/data/mappers/NewsDataToNewsDomain.kt
+++ app/src/main/java/com/wizeline/mobilenews/data/mappers/NewsDataToNewsDomain.kt
@@ -1,5 +1,29 @@
 package com.wizeline.mobilenews.data.mappers
 
-class NewsDataToNewsDomain() {
-    // TODO: Add mapper with Article Object
+import com.wizeline.mobilenews.data.models.NetworkResults
+import com.wizeline.mobilenews.data.models.NewsRaw
+import com.wizeline.mobilenews.domain.models.Article
+import com.wizeline.mobilenews.domain.models.CommunityArticle
+import com.wizeline.mobilenews.domain.models.NewsArticle
+
+abstract class NewsDataToNewsDomain<T, R> {
+
+    fun mapNetworkResult(
+        items: List<T>?,
+        mapTo: (T) -> R,
+    ): NetworkResults<List<R>> {
+        return try {
+            if(items.isNullOrEmpty())
+                error("Null list")
+            else{
+                val mappedItems = items.map(mapTo)
+                NetworkResults.Success(mappedItems)
+            }
+        } catch (e: Exception) {
+            error(e.message ?: e.toString())
+        }
+    }
+    private fun <T> error(errorMessage: String): NetworkResults<T> =
+        NetworkResults.Error("Mapping failed: $errorMessage")
+
 }
