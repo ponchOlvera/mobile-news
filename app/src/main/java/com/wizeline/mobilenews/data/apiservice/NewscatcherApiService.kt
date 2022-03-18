2c776250d7316141b4d2e32752832e9e2c09bfda
Luis Olvera <luis.olvera@wizeline.com>
2022-03-18 13:54:24 -0600

    Only display articles related to Android OS #35
    + Removed the getAllNews() service  call.
    + Added a default search label for the main screen.

--- app/src/main/java/com/wizeline/mobilenews/data/apiservice/NewscatcherApiService.kt
+++ app/src/main/java/com/wizeline/mobilenews/data/apiservice/NewscatcherApiService.kt
@@ -1,5 +1,6 @@
 package com.wizeline.mobilenews.data.apiservice
 
+import com.wizeline.mobilenews.data.apiservice.NewscatcherApi.DEFAULT_ANDROID
 import com.wizeline.mobilenews.data.apiservice.NewscatcherApi.LANGUAGE
 import com.wizeline.mobilenews.data.apiservice.NewscatcherApi.LATEST_HEADLINES
 import com.wizeline.mobilenews.data.apiservice.NewscatcherApi.SEARCH_NEWS
@@ -21,19 +22,7 @@ interface NewscatcherApiService {
         @Query("topic")
         topic: String = TOPIC,
         @Query("q")
-        query: String,
-        @Query("page_size")
-        pageSize: Int?,
-        @Query("page")
-        page: Int?
-    ): Response<NewsRaw>
-
-    @GET(LATEST_HEADLINES)
-    suspend fun getAllNews(
-        @Query("lang")
-        lang: String = LANGUAGE,
-        @Query("topic")
-        topic: String = TOPIC,
+        query: String = DEFAULT_ANDROID,
         @Query("page_size")
         pageSize: Int?,
         @Query("page")
